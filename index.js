3c1d04be52fe2f9240e3e82c00bd335c16514abf
Omar Aviles <edaviles@microsoft.com>
2022-03-03 13:40:16 -0600

    Log message updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const messageValue = core.getInput('message');
-  console.log(`Hello ${messageValue}!`);
+  console.log(`OA-Demo: ${messageValue}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
